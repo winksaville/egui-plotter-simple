cabb8184f441e3a1e19fee71a2b30ef301e2a89d
Wink Saville <wink@saville.com>
2023-10-01 14:29:06 -0700

    feat: Initial Commit
    This does not compile, I get the following error mismatched types:
    
        Compiling plotters-egui-exmpl-parabola v0.1.0 (/home/wink/prgs/rust/myrepos/plotters-egui-exmpl-parabola)
     error[E0308]: mismatched types
        --> src/main.rs:36:41
         |
     36  |             let root = EguiBackend::new(ui).into_drawing_area();
         |                        ---------------- ^^ expected `&Ui`, found `&mut Ui`
         |                        |
         |                        arguments to this function are incorrect
         |
         = note: `Ui` and `egui::ui::Ui` have similar names, but are actually distinct types
     note: `Ui` is defined in crate `egui`
        --> /home/wink/.cargo/registry/src/index.crates.io-6f17d22bba15001f/egui-0.23.0/src/ui.rs:31:1
         |
     31  | pub struct Ui {
         | ^^^^^^^^^^^^^
     note: `egui::ui::Ui` is defined in crate `egui`
        --> /home/wink/.cargo/registry/src/index.crates.io-6f17d22bba15001f/egui-0.22.0/src/ui.rs:31:1
         |
     31  | pub struct Ui {
         | ^^^^^^^^^^^^^
         = note: perhaps two different versions of crate `egui` are being used?
     note: associated function defined here
        --> /home/wink/.cargo/registry/src/index.crates.io-6f17d22bba15001f/egui-plotter-0.3.0/src/backend.rs:200:12
         |
     200 |     pub fn new(ui: &'a Ui) -> Self {
         |            ^^^
    
     For more information about this error, try `rustc --explain E0308`.
     error: could not compile `plotters-egui-exmpl-parabola` (bin "plotters-egui-exmpl-parabola") due to previous error

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use eframe::egui::{self, CentralPanel, Visuals};
+use egui_plotter::EguiBackend;
+use plotters::prelude::*;
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Simple Example",
+        native_options,
+        Box::new(|cc| Box::new(Simple::new(cc))),
+    )
+    .unwrap();
+}
+
+struct Simple;
+
+impl Simple {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Disable feathering as it causes artifacts
+        let context = &cc.egui_ctx;
+
+        context.tessellation_options_mut(|tess_options| {
+            tess_options.feathering = false;
+        });
+
+        // Also enable light mode
+        context.set_visuals(Visuals::light());
+
+        Self
+    }
+}
+
+impl eframe::App for Simple {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            let root = EguiBackend::new(ui).into_drawing_area();
+            root.fill(&WHITE).unwrap();
+            let mut chart = ChartBuilder::on(&root)
+                .caption("y=x^2", ("sans-serif", 50).into_font())
+                .margin(5)
+                .x_label_area_size(30)
+                .y_label_area_size(30)
+                .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)
+                .unwrap();
+
+            chart.configure_mesh().draw().unwrap();
+
+            chart
+                .draw_series(LineSeries::new(
+                    (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+                    &RED,
+                ))
+                .unwrap()
+                .label("y = x^2")
+                .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+            chart
+                .configure_series_labels()
+                .background_style(&WHITE.mix(0.8))
+                .border_style(&BLACK)
+                .draw()
+                .unwrap();
+
+            root.present().unwrap();
+        });
+    }
+}
